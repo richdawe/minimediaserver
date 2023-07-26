4dfbb5968d69b67cb41214c8c57eedf4d1b4a007
Richard Dawe <richdawe@gmail.com>
2023-07-26 19:08:56 +0100

    Bugfix: graceful shutdown on Ctrl+C

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -28,7 +32,27 @@ func main() {
 	// TODO: need a config file for specifying HTTP server options
 	e.Use(middleware.Timeout())
 	e.Use(middleware.Logger())
-	e.Logger.Fatal(e.Start(config.Addr))
+
+	// Start server
+	// https://echo.labstack.com/docs/cookbook/graceful-shutdown
+	// https://medium.com/@mokiat/proper-http-shutdown-in-go-bd3bfaade0f2
+	go func() {
+		if err := e.Start(config.Addr); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal(fmt.Sprintf("shutting down the server: %s", err))
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+		e.Close()
+	}
 
 	fmt.Println("DONE")
 }
